97b0f3ec2f427a59593534c55a2c1cd5e498bcaf
mingyeongwon <wonmingy@gmail.com>
2024-10-18 01:06:27 +0900

    vue에 있는 로그인 페이지 사용하도록 변경

--- src/apis/axiosConfig.js
+++ src/apis/axiosConfig.js
@@ -5,14 +5,3 @@ axios.defaults.baseURL = "http://localhost:8181/api";
 
 // withCredentials : 서로 다른 도메인에서 쿠키 & authorization 요청 허용 
 axios.defaults.withCredentials = true;
-
-axios.interceptors.response.use(
-    (response) => response,
-    (err) => {
-        if(err.response && err.response.status === 401) {
-            console.error("인증 실패");
-            window.location.href = "http://localhost:8181/login";
-        }
-        return Promise.reject(err);
-    }
-);
