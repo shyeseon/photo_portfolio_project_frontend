f91e1159452da771a90b758783dcbcaecd0717be
shyeseon <gptjs1222@gmail.com>
2024-10-22 13:46:37 +0900

    fix: project router link

--- src/router/Image.js
+++ src/router/Image.js
@@ -1,11 +1,11 @@
 const routes = [
     {
-        path: '/PhotoListPage/PhotoList.vue',
+        path: '/PhotoListPage/PhotoList/:categoryId/:subCategoryId?',
         name: "photoList",
         component: () =>  import(/* webpackChunkName: "Image" */ '@/views/PhotoListPage/PhotoList.vue')
       },
     {         
-        path: '/PhotoListPage/DetailList.vue',
+        path: '/PhotoListPage/DetailList/:projectId',
         name: 'DetailList',
         component: () =>  import(/* webpackChunkName: "Image" */ '@/views/PhotoListPage/DetailList.vue')
     },
