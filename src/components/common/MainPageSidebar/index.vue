e3d03f3dbd7080a8dfac872ac6d18214611b0d8b
mingyeongwon <wonmingy@gmail.com>
2024-11-03 19:18:07 +0900

    refactor: main navBar에서 admin 로그인 상태만 표시
    store.state.id 사용

--- src/components/common/MainPageSidebar/index.vue
+++ src/components/common/MainPageSidebar/index.vue
@@ -49,7 +49,7 @@
           <RouterLink class="nav-link mb-3" to="/contact" @click="menuItemClicked">Contact</RouterLink>
         </li>
         <!-- Admin 메뉴 항목 표시 -->
-        <li class="nav-item" v-if="isAdmin!=`anonymousUser`">
+        <li class="nav-item" v-if="store.state.id">
           <RouterLink class="nav-link mb-3" to="/Admin/ManageImages" @click="menuItemClicked">Admin</RouterLink>
         </li>
       </ul>
@@ -65,9 +65,10 @@
 import axios from "axios";
 import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
+import store from "@/store";
 
 const categories = ref([]);
-const isAdmin = ref(false); // 관리자인지 여부를 저장
+// const isAdmin = ref(false); // 관리자인지 여부를 저장
 const route = useRoute();
 
 // 페이지에 따라 적절한 카테고리를 가져오는 함수
@@ -84,19 +85,19 @@ const getCategory = async () => {
   }
 };
 
-// 세션 확인 함수
-const checkSession = async () => {
-  try {
-    const response = await axios.get("/loginSucess");
-    isAdmin.value = response.data.isAdmin; // 관리자 여부를 isAdmin 변수에 저장
-  } catch (error) {
-    console.error("세션 확인 실패:", error);
-  }
-};
+// // 세션 확인 함수
+// const checkSession = async () => {
+//   try {
+//     const response = await axios.get("/loginSucess");
+//     isAdmin.value = response.data.isAdmin; // 관리자 여부를 isAdmin 변수에 저장
+//   } catch (error) {
+//     console.error("세션 확인 실패:", error);
+//   }
+// };
 
 // 페이지가 로드될 때 세션을 확인하고 카테고리를 가져옵니다.
 onMounted(async () => {
-  await checkSession();
+  // await checkSession();
   await getCategory();
 });
 
