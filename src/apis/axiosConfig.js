d0c5128656997e8f0dcc4b2ed622b9d159b3f746
sunghwan kwon <sunghwan.ian.kwon@gmail.com>
2024-11-09 21:25:44 +0900

    chore: 배포된 url 포트로 변경

--- src/apis/axiosConfig.js
+++ src/apis/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // 기본 경로 설정
-axios.defaults.baseURL = "http://localhost:8181/api";
+axios.defaults.baseURL = "https://photo-portfolio-project-backend-450062450110.us-central1.run.app/api";
 
 // withCredentials : 서로 다른 도메인에서 쿠키 & authorization 요청 허용 
 axios.defaults.withCredentials = true;  
